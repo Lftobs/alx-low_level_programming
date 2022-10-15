0fb556018f0d57cda7f41fd8ed37e04ae71534a9
Lftobs <tobbytobs1@gmail.com>
2022-10-14 17:03:34 -0700

    9 fixed

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
 /**
- * main - Entry
+ * main - Entry point
  * Return: 0
  */
+
 int main(void)
 {
-	int n = 0;
+	int n;
 
-	while (n < 10)
+	for (n = '0'; n <= '9'; d++)
 	{
-		putchar((n % 10) + '0');
-		n++;
-		if (n == '9')
-			continue;
-		putchar(',');
-		putchar(' ');
+		putchar(n);
+		if (n != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
+
 	return (0);
-}
