6d55c4c651b16614b8d53baf37c3942ef9d9dd7f
Lftobs <tobbytobs1@gmail.com>
2022-10-20 17:55:19 -0700

    7-fixed

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,18 +6,22 @@
  */
 void print_diagonal(int n)
 {
-	int i = 0;
+	int i, j;
 
 	if (n > 0)
 	{
-		do {
-			int j;
-
+		for (i = 0; i < n; i++)
+		{
 			for (j = 0; j < i; j++)
 				_putchar(' ');
 			_putchar('\\');
-			i++;
+
+			if (i == n - 1)
+				continue;
+
 			_putchar('\n');
-		} while (i < n);
+		}
 	}
+
+	_putchar('\n');
 }
