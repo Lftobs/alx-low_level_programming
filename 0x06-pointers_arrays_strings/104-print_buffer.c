07faa19d41368c0d5197100911bf825bb86ce108
Lftobs <tobbytobs1@gmail.com>
2022-10-27 14:11:20 -0700

    104

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_buffer - Entry
+ * @b: IFC
+ * @size: IFC
+ * Return: 0
+ */
+void print_buffer(char *b, int size)
+{
+	int i, j, k;
+
+	if (size <= 0)
+		printf("\n");
+	else
+	{
+		for (j = 0; j < size; j += 10)
+		{
+			printf("%.8x:", j);
+			for (k = j; k < j + 10; k++)
+			{
+				if (k % 2 == 0)
+					printf(" ");
+				if (k < size)
+					printf("%.2x", *(b + k));
+				else
+					printf("  ");
+			}
+			printf(" ");
+			for (i = j; i < j + 10; i++)
+			{
+				if (i >= size)
+					break;
+				if (*(b + i) < 32 || *(b + i) > 126)
+					printf("%c", '.');
+				else
+					printf("%c", *(b + i));
+			}
+			printf("\n");
+		}
+	}
+}
