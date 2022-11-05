945258a5b7a9351c9fd10d910bcad0177851a1e4
Lftobs <tobbytobs1@gmail.com>
2022-11-04 17:19:43 -0700

    1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -4,10 +4,12 @@
  * main - entry
  * @argc: ifc
  * @argv: ifc
+ * Return: 0
  */
 
-int main(int argc, char *argv[] __attribute__((unused)))
+int main(int argc, char *argv[])
 {
-	printf("%d\n", argc - 1);
+	if (argv[0])
+		printf("%d\n", argc - 1);
 	return (0);
 }
