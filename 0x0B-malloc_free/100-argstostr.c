91f12de1fd7daaf4a273b5eaa1ba51a4cdbf4b30
Lftobs <tobbytobs1@gmail.com>
2022-11-08 12:11:52 -0800

    100

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,51 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * argstostr - entry
+ * @ac: ifc
+ * @av: ifc
+ * Return: 0
+ */
+char *argstostr(int ac, char **av)
+{
+	char *a;
+	int k, i, j, l;
+
+	if (ac == 0)
+		return (NULL);
+
+	for (k = i = 0; i < ac; i++)
+	{
+		if (av[i] == NULL)
+			return (NULL);
+
+		for (j = 0; av[i][j] != '\0'; j++)
+			k++;
+		k++;
+	}
+
+	a = malloc((k + 1) * sizeof(char));
+
+	if (a == NULL)
+	{
+		free(a);
+		return (NULL);
+	}
+
+	for (i = j = l = 0; l < k; j++, l++)
+	{
+		if (av[i][j] == '\0')
+		{
+			a[ia] = '\n';
+			i++;
+			l++;
+			j = 0;
+		}
+		if (l < c - 1)
+			a[ia] = av[i][j];
+	}
+	a[ia] = '\0';
+
+	return (a);
+}
