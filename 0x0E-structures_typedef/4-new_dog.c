23f02d9903c166cdc1e2285412349355aeb4e920
Lftobs <tobbytobs1@gmail.com>
2022-11-14 16:39:53 -0800

    0 - 5

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,51 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * new_dog - Entry
+ * @name: name 
+ * @age: age 
+ * @owner: owner 
+ * Return: 0
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog_new;
+	int i, lname, lowner;
+
+	dog_new = malloc(sizeof(*dog_new));
+	if (dog_new == NULL || !(name) || !(owner))
+	{
+		free(dog_new);
+		return (NULL);
+	}
+
+	for (lname = 0; name[lname]; lname++)
+		;
+
+	for (lowner = 0; owner[lowner]; lowner++)
+		;
+
+	dog_new->name = malloc(lname + 1);
+	dog_new->owner = malloc(lowner + 1);
+
+	if (!(dog_new->name) || !(dog_new->owner))
+	{
+		free(dog_new->owner);
+		free(dog_new->name);
+		free(dog_new);
+		return (NULL);
+	}
+
+	for (i = 0; i < lname; i++)
+		dog_new->name[i] = name[i];
+	dog_new->name[i] = '\0';
+
+	dog_new->age = age;
+
+	for (i = 0; i < lowner; i++)
+		dog_new->owner[i] = owner[i];
+	dog_new->owner[i] = '\0';
+
+	return (dog_new);
+}
