03548c54682237a0eb2c25134ccee34755cb5455
Lftobs <tobbytobs1@gmail.com>
2022-11-24 12:10:47 -0800

    1f

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -13,11 +13,12 @@ size_t list_len(const list_t *h)
 	if (h == NULL)
 		return (0);
 
-	do {
+	while (h->next != NULL)
+	{
 		h = h->next;
 
 		count++;
-	} while (h->next != NULL);
+	}
 
 	return (count);
 }
